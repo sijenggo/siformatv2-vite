7efc1a3f1078965265e88e13654cd1168c046470
sijenggo <fauzanadimas@gmail.com>
2025-01-30 21:53:33 +0700

    jadi tinggal bikin buser current liat apaansi

--- src/pages/Papan.jsx
+++ src/pages/Papan.jsx
@@ -3,6 +3,7 @@ import { ambil_data, formattedDate } from "./control/services";
 import { useQuery } from "@tanstack/react-query";
 import { Col, Container, Row} from "react-bootstrap";
 import AutoScrollTable from "./control/autoscroll";
+import { useState } from "react";
 
 const fetchAntrian = async (id_loket) => {
     let today = new Date();
@@ -11,12 +12,14 @@ const fetchAntrian = async (id_loket) => {
     today.setDate(dite.getDate() - 1);*/
     let date = formattedDate(today);
     return await ambil_data(
-      `SELECT * FROM tmp_antrian ORDER BY id_antrian DESC`
+      `SELECT * FROM tmp_antrian LEFT JOIN sys_loket ON tmp_antrian.id_loket = sys_loket.id_loket ORDER BY id_antrian DESC`
     );
 };
 
 
 const Papan = () =>{
+    const [nomorAntrian, setNomorAntrian] = useState('-');
+    const [ket, setKet] = useState('-');
     const { data: antrianPerdata = [], isLoading: loadingPerdata } = useQuery({
       queryKey: ["antrian", 2],
       queryFn: () => fetchAntrian(2),
@@ -46,6 +49,11 @@ const Papan = () =>{
         console.log("End");
     }
 
+    const handleOnStart = (nomor_antrian, ket) =>{
+        setNomorAntrian(nomor_antrian);
+        setKet(ket);
+    }
+
     return(
         <>
             <div className='limiter'>
@@ -56,8 +64,9 @@ const Papan = () =>{
                                 <Row className="h-100 py-2">
                                     <Col md={3} className="h-100">
                                         <div className="main-display d-flex flex-column justify-content-evenly align-items-center p-1 h-100">
-                                            <h1 className="fs-big">A - 2</h1>
-                                            <h4>PTSP Perdata</h4>
+                                            <h1 className="text-center">Antrian PTSP Pengadilan Negeri Banyumas</h1>
+                                            <h1 className="fs-big">{nomorAntrian}</h1>
+                                            <h4>{ket}</h4>
                                         </div>
                                     </Col>
                                     <Col md={9} className="h-100">
@@ -80,13 +89,41 @@ const Papan = () =>{
                                                             <h5>Meja PTSP Perdata</h5>
                                                         </div>
                                                         <div className="main-content">
-                                                            <AutoScrollTable antrian={antrianPerdata} loading={loadingPerdata} handleOnEnd={handleOnEnd} />
+                                                            <AutoScrollTable antrian={antrianPerdata} loading={loadingPerdata} handleOnStart={handleOnStart} handleOnEnd={handleOnEnd} />
                                                         </div>
                                                     </Col>
-                                                    <Col>2</Col>
-                                                    <Col>3</Col>
-                                                    <Col>4</Col>
-                                                    <Col>5</Col>
+                                                    <Col className="h-100 d-flex flex-column align-items-center">
+                                                        <div>
+                                                            <h5>Meja PTSP Pidana</h5>
+                                                        </div>
+                                                        <div className="main-content">
+                                                            <AutoScrollTable antrian={antrianPidana} loading={loadingPidana} handleOnStart={handleOnStart} handleOnEnd={handleOnEnd} />
+                                                        </div>
+                                                    </Col>
+                                                    <Col className="h-100 d-flex flex-column align-items-center">
+                                                        <div>
+                                                            <h5>Meja PTSP Hukum</h5>
+                                                        </div>
+                                                        <div className="main-content">
+                                                            <AutoScrollTable antrian={antrianHukum} loading={loadingHukum} handleOnStart={handleOnStart} handleOnEnd={handleOnEnd} />
+                                                        </div>
+                                                    </Col>
+                                                    <Col className="h-100 d-flex flex-column align-items-center">
+                                                        <div>
+                                                            <h5>Meja PTSP Umum</h5>
+                                                        </div>
+                                                        <div className="main-content">
+                                                            <AutoScrollTable antrian={antrianUmum} loading={loadingUmum} handleOnStart={handleOnStart} handleOnEnd={handleOnEnd} />
+                                                        </div>
+                                                    </Col>
+                                                    <Col className="h-100 d-flex flex-column align-items-center">
+                                                        <div>
+                                                            <h5>Meja PTSP <i>E - Court</i></h5>
+                                                        </div>
+                                                        <div className="main-content">
+                                                            <AutoScrollTable antrian={antrianEcourt} loading={loadingEcourt} handleOnStart={handleOnStart} handleOnEnd={handleOnEnd} />
+                                                        </div>
+                                                    </Col>
                                                 </Row>
                                             </Container>
                                         </div>
--- src/pages/control/autoscroll.jsx
+++ src/pages/control/autoscroll.jsx
@@ -1,54 +1,72 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useMemo, useState } from "react";
 import { animateScroll as scroll } from "react-scroll";
 import { Table, Button } from "react-bootstrap";
 import jingle from "../../assets/audios/jingle.mp3";
 import { prepText } from "./services";
 import { useSpeech, useQueue } from "react-text-to-speech";
 
-const ItemPanggil = ({id, id_loket, nomor_antrian, onSpeechEnd }) => {
-    const playjingle = useRef(new Audio(jingle)); // Gunakan useRef untuk menghindari reinitiasi
-    let teks = prepText(id_loket, nomor_antrian);
-    const {speechStatus, isInQueue, start, stop } = useSpeech({
+const ItemPanggil = ({ id, id_loket, nomor_antrian, ket, onSpeechEnd, onSpeechStart }) => {
+    const playjingle = useMemo(() => new Audio(jingle), []); // Gunakan useMemo agar tetap konsisten
+    const [isPlayingJingle, setIsPlayingJingle] = useState(false);
+    const teks = prepText(id_loket, nomor_antrian);
+
+    const { speechStatus, isInQueue, start, stop } = useSpeech({
         text: teks,
         autoPlay: false,
         preserveUtteranceQueue: true,
         volume: 1,
         voiceURI: "Google Bahasa Indonesia",
         lang: "id-ID",
-        onStop: () => {
-            onSpeechEnd();
-        }
+        onStop: onSpeechEnd,
     });
 
-    const {
-        queue,
-    } = useQueue();
+    const { queue } = useQueue();
+
+    useEffect(() => {
+        if (!playjingle) return;
+
+        const handlePlay = () => setIsPlayingJingle(true);
+        const handlePause = () => setIsPlayingJingle(false);
+        const handleEnded = () => setIsPlayingJingle(false);
+
+        playjingle.addEventListener("play", handlePlay);
+        playjingle.addEventListener("pause", handlePause);
+        playjingle.addEventListener("ended", handleEnded);
+
+        return () => {
+            playjingle.removeEventListener("play", handlePlay);
+            playjingle.removeEventListener("pause", handlePause);
+            playjingle.removeEventListener("ended", handleEnded);
+        };
+    }, [playjingle]);
 
     const handleStart = () => {
-        if(queue.length > 0){
+        if (queue.length > 0) {
             start();
-        }else{
-            let play = playjingle.current.play();
-            if (play !== undefined) {
-                play
+        } else {
+            const playPromise = playjingle.play();
+            if (playPromise !== undefined) {
+                playPromise
                     .then(() => {
                         setTimeout(() => {
                             start();
                         }, 2150);
                     })
                     .catch((error) => {
-                        console.error("Error sound!");
+                        console.error("Error playing sound:", error);
+                        start();
                     });
             } else {
                 start();
             }
         }
+        onSpeechStart(nomor_antrian, ket);
     };
 
     const handleStop = () => {
         stop();
-        playjingle.current.pause();
-        playjingle.current.currentTime = 0;
+        playjingle.pause();
+        playjingle.currentTime = 0;
     };
 
     useEffect(() => {
@@ -57,37 +75,27 @@ const ItemPanggil = ({id, id_loket, nomor_antrian, onSpeechEnd }) => {
                 stop();
             }, teks.length * 100);
         }
-    }, [speechStatus]);
+    }, [speechStatus, queue, stop, teks.length]);
 
     return (
         <>
-            {speechStatus !== "started" || !isInQueue ? (
-                <Button
-                    id={id}
-                    onClick={handleStart}
-                    variant="outline-primary"
-                    size="sm"
-                    className="non-pad"
-                >
+            {!isPlayingJingle && speechStatus !== "started" && !isInQueue ? (
+                <Button id={id} onClick={handleStart} variant="outline-primary" size="sm" className="non-pad">
                     <i className="fa-solid fa-circle-play fa-beat-fade fa-2xs"></i>
                 </Button>
             ) : (
-                <Button
-                    id={id}
-                    onClick={handleStop}
-                    variant="outline-danger"
-                    size="sm"
-                    className="non-pad"
-                >
-                    <i className="fa-solid fa-circle-stop fa-beat-fade fa-2xs"></i>
+                <Button id={id} onClick={handleStop} variant="outline-danger" size="sm" className="non-pad">
+                    <i className="fa-solid fa-circle-stop fa-beat-fade"></i>
                 </Button>
             )}
         </>
     );
 };
 
-const AutoScrollTable = ({ antrian, loading, handleOnEnd }) => {
+
+const AutoScrollTable = ({ antrian, loading, handleOnEnd, handleOnStart }) => {
     const tableRef = useRef(null);
+    const containerId = `scrollable-table-${Math.random().toString(36).substr(2, 9)}`; // ID unik untuk tiap komponen
 
     useEffect(() => {
         if (antrian.length > 0) {
@@ -98,13 +106,13 @@ const AutoScrollTable = ({ antrian, loading, handleOnEnd }) => {
 
                 if (scrollDown) {
                     scroll.scrollToBottom({
-                        containerId: "scrollable-table",
+                        containerId,
                         duration: antrian.length * 1000, // Sesuaikan kecepatan scrolling
                         smooth: "linear"
                     });
                 } else {
                     scroll.scrollToTop({
-                        containerId: "scrollable-table",
+                        containerId,
                         duration: antrian.length * 1000,
                         smooth: "linear"
                     });
@@ -116,10 +124,10 @@ const AutoScrollTable = ({ antrian, loading, handleOnEnd }) => {
             const interval = setInterval(scrollTable, antrian.length * 1000 + 2000); // Beri jeda sebelum scroll balik
             return () => clearInterval(interval);
         }
-    }, [antrian]);
+    }, [antrian, containerId]);
 
     return (
-        <div id="scrollable-table" ref={tableRef} style={{ overflowY: "hidden", maxHeight: "200px" }}>
+        <div id={containerId} ref={tableRef} style={{ overflowY: "hidden", maxHeight: "200px" }}>
             {loading ? (
                 <p>Loading data...</p>
             ) : antrian.length > 0 ? (
@@ -129,7 +137,7 @@ const AutoScrollTable = ({ antrian, loading, handleOnEnd }) => {
                             <tr key={item.id_antrian}>
                                 <td className="text-center">{item.nomor_antrian}</td>
                                 <td className="text-center">
-                                    <ItemPanggil id={item.id_antrian} id_loket={item.id_loket} nomor_antrian={item.nomor_antrian} onSpeechEnd={handleOnEnd} />
+                                    <ItemPanggil id={item.id_antrian} id_loket={item.id_loket} nomor_antrian={item.nomor_antrian} ket={item.ket} onSpeechStart={handleOnStart} onSpeechEnd={handleOnEnd} />
                                 </td>
                             </tr>
                         ))}
@@ -141,5 +149,6 @@ const AutoScrollTable = ({ antrian, loading, handleOnEnd }) => {
         </div>
     );
 };
+
 
 export default AutoScrollTable;
